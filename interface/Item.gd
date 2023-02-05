de4b1087d1366eeb591150033d9a6c5bda4736a3
karlos <k.raffay@protonmail.com>
2023-02-05 22:54:18 +0100

    auto login znova

--- interface/Item.gd
+++ interface/Item.gd
@@ -10,12 +10,10 @@ onready var amount_label := $MarginContainer/HBoxContainer/AmountLabel
 onready var name_label := $MarginContainer/HBoxContainer/NameLabel
 onready var tooltip_timer := $TooltipTimer
 
-
 func _ready() -> void:
 	tooltip_timer.connect("timeout", self, "_request_tooltip")
 	connect("mouse_entered", tooltip_timer, "start")
 	connect("mouse_exited", tooltip_timer, "stop")
-
 
 func display_item(unique_id: String, amount: int) -> void:
 	item_unique_id = unique_id
@@ -24,7 +22,6 @@ func display_item(unique_id: String, amount: int) -> void:
 	texture_rect.texture = data.icon
 	name_label.text = data.display_name
 	amount_label.text = str(amount).pad_zeros(2)
-
 
 func _request_tooltip() -> void:
 	if item_unique_id:
