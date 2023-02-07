ed1e6b59d5710319cf6416fe556052f4cc4c1e62
karlos <k.raffay@protonmail.com>
2023-02-07 23:42:21 +0100

    Big merge ig

--- interface/Inventory.gd
+++ interface/Inventory.gd
@@ -10,8 +10,14 @@ func _ready() -> void:
 	# INFO: Funguje jenom kdyz spustite tuto scenu, jinak ne
 	if get_parent() == get_tree().root:
 		var test_inventory := Inventory.new()
-		test_inventory.add_item("extinguisher")
+		test_inventory.add_item("extinguisher", 1)
+		test_inventory.add_item("key", 1)
+		test_inventory.add_item("gear", 1)
+		test_inventory.add_item("coin", 1)
 		set_inventory(test_inventory)
+	#inventory.add_item("extinguisher", 1)
+	#_update_items_display()
+	pass
 
 func set_inventory(new_inventory: Inventory) -> void:
 	if inventory != new_inventory:
@@ -33,8 +39,22 @@ func _input(event):
 	if event is InputEventKey:
 		if event.scancode == KEY_1:
 			if inventory.is_item("extinguisher"):
-				inventory.remove_item("extinguisher")
+				inventory.remove_item("extinguisher", 1)
 		elif event.scancode == KEY_2:
-			pass
+			if inventory.is_item("key"):
+				inventory.remove_item("key", 1)
 		elif event.scancode == KEY_3:
-			pass
+			if inventory.is_item("gear"):
+				inventory.remove_item("gear", 1)
+		elif event.scancode == KEY_4:
+			if inventory.is_item("coin"):
+				inventory.remove_item("coin", 1)
+				
+func _add_random_item() -> void:
+	var item_unique_id: String = ItemDatabase.ITEMS.keys()[randi() % ItemDatabase.ITEMS.keys().size()]
+	inventory.add_item(item_unique_id, 1)
+
+
+func _remove_random_item() -> void:
+	if inventory.items:
+		inventory.remove_item(inventory.items.keys()[randi() % inventory.items.keys().size()])
