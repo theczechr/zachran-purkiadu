f824399fc92a49c55d05aea0efd115aa37c3bba3
karlos <k.raffay@protonmail.com>
2023-02-09 11:25:54 +0100

    Base interakce s inventarem

--- interface/Inventory.gd
+++ interface/Inventory.gd
@@ -10,14 +10,10 @@ func _ready() -> void:
 	# INFO: Funguje jenom kdyz spustite tuto scenu, jinak ne
 	if get_parent() == get_tree().root:
 		var test_inventory := Inventory.new()
-		test_inventory.add_item("extinguisher", 1)
-		test_inventory.add_item("key", 1)
-		test_inventory.add_item("gear", 1)
-		test_inventory.add_item("coin", 1)
+		test_inventory.add_item("extinguisher")
 		set_inventory(test_inventory)
-	#inventory.add_item("extinguisher", 1)
-	#_update_items_display()
-	pass
+		
+	inventory.add_item("extinguisher")
 
 func set_inventory(new_inventory: Inventory) -> void:
 	if inventory != new_inventory:
@@ -41,14 +37,9 @@ func _input(event):
 			if inventory.is_item("extinguisher"):
 				inventory.remove_item("extinguisher", 1)
 		elif event.scancode == KEY_2:
-			if inventory.is_item("key"):
-				inventory.remove_item("key", 1)
+			pass
 		elif event.scancode == KEY_3:
-			if inventory.is_item("gear"):
-				inventory.remove_item("gear", 1)
-		elif event.scancode == KEY_4:
-			if inventory.is_item("coin"):
-				inventory.remove_item("coin", 1)
+			pass
 				
 func _add_random_item() -> void:
 	var item_unique_id: String = ItemDatabase.ITEMS.keys()[randi() % ItemDatabase.ITEMS.keys().size()]
