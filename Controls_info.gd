3fdf26bae33e0b55fc315e9057c9f47c3ebbb3f0
Adam Kopřiva <Admin.Nuklear@seznam.cz>
2023-02-12 14:53:54 +0100

    fix

--- Controls_info.gd
+++ Controls_info.gd
@@ -2,5 +2,5 @@ extends Control
 
 func _physics_process(delta):
 	if Input.is_key_pressed(KEY_SPACE):
-		get_tree().change_scene("res://Game.tscn")
+		get_tree().change_scene("res://Level1.tscn")
 	
