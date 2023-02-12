82d753e926fa47c40b35cb2623abe90c3db3a39a
Adam Kopřiva <Admin.Nuklear@seznam.cz>
2023-02-12 19:43:27 +0100

    funkcni load scen

--- ControlsInfo.gd
+++ ControlsInfo.gd
@@ -1,6 +1,10 @@
 extends Control
 
+var _save = SaveGame
+
 func _physics_process(delta):
 	if Input.is_key_pressed(KEY_SPACE):
-		get_tree().change_scene("res://Level1.tscn")
-	
+		if SaveGame.save_exists():
+			get_tree().change_scene("res://EmptySceen.tscn")
+		else:
+			get_tree().change_scene("res://Level1.tscn")
