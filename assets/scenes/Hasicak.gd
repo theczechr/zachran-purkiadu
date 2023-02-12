fd81acc9361b6bde3824ccb4aaca26820f6db54e
karlos <k.raffay@protonmail.com>
2023-02-12 20:33:06 +0100

    Pickup hasicaku, inventar

--- assets/scenes/Hasicak.gd
+++ assets/scenes/Hasicak.gd
@@ -1,8 +1,17 @@
 extends KinematicBody2D
 
-func _on_Area2D_body_entered(body):
-	pass # Replace with function body.
+signal pickup_hasicak
 
+var status = false
+func _unhandled_input(event):
+	if Input.is_key_pressed(KEY_E) and status:
+		emit_signal("pickup_hasicak")
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player"):
+		body.set("show_prompt", true)
+		status = true
 
 func _on_Area2D_body_exited(body):
-	pass # Replace with function body.
+	body.set("show_prompt", false)
+	status = false
