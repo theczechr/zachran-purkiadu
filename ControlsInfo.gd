babbbaab10d494c6f9e849bd60cbec7b847070a6
Adam Kopřiva <Admin.Nuklear@seznam.cz>
2023-02-13 23:58:53 +0100

    no prints

--- ControlsInfo.gd
+++ ControlsInfo.gd
@@ -5,10 +5,10 @@ var loaded_scene = true
 func _physics_process(delta):
 	if Input.is_key_pressed(KEY_SPACE):
 		if SaveGame.save_exists():
-			print("brudda get to empty sceene ")
+			#print("brudda get to empty sceene ")
 			loaded_scene = false
 			get_tree().change_scene("res://EmptySceen.tscn")
 		else:
-			print("went straight to level one yeah")
+			#print("went straight to level one yeah")
 			get_tree().change_scene("res://Level1.tscn")
 	print(loaded_scene)
