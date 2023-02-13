b3b6185ca02df6742b8196cd6fc78a366da25e0c
Ondrejtra <koala.sopel@gmail.com>
2023-02-13 19:26:35 +0100

    Actually funkční ukládání na server + varování

--- login/Firebase.gd
+++ login/Firebase.gd
@@ -15,17 +15,17 @@ func get_progress(username: String, http: HTTPRequest) -> int:
 	http.request(request, [], false, HTTPClient.METHOD_GET)
 	var result := yield(http, "request_completed") as Array
 	print(result)
-	print(PoolByteArray(result[3]).get_string_from_utf8())
+	var return_value = PoolByteArray(result[3]).get_string_from_utf8()
+	#print("get ", int(return_value), ", ", str(return_value), ", ", return_value)
 
 	if result[1] == 200:
-		return int(PoolByteArray([result[3]]).get_string_from_utf8())
+		return int(return_value)
 	return 0
 
 func set_progress(username: String, progress: String, http: HTTPRequest) -> bool:
 	var rhash := (username + progress + SALT).sha256_text()
 	var request = SERVER_URL + "/?username=" + username.http_escape() + "&hash=" + rhash.http_escape() + "&progress=" + progress.http_escape()
-
 	http.request(request, [], false, HTTPClient.METHOD_PUT)
 	var result := yield(http, "request_completed") as 	Array
-	print(result)
 	return result[1] == 200
+
