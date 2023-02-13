b177796e059f9713d4405850461735446fbe9c44
theczechr <robinjarusek@gmail.com>
2023-02-13 22:25:28 +0100

    wordle skoro done

--- minigames/wordle/Wordle.gd
+++ minigames/wordle/Wordle.gd
@@ -1,13 +1,18 @@
 extends Control
 
 func _ready():
-	$AspectRatioContainer3/AspectRatioContainer/line_container.connect("game_over", self, "game_over")
+	$ReferenceRect/line_container.connect("game_over", self, "game_over")
 
 func game_over(word, guessed):
-	$AspectRatioContainer2/Label.text = word
+	$ReferenceRect/Label.text = "Odemknuto!" if guessed else "Špatné heslo!"
 	$AnimationPlayer.play("fade_in")
-	yield(get_tree().create_timer(3), "timeout")
-	if guessed:
+	$ReferenceRect/ColorRect3.color = "1e962b" if guessed else "bd1616"
+	yield(get_tree().create_timer(4), "timeout")
+	if guessed:	
 		get_tree().change_scene("res://Level4.tscn")
 	else:
 		get_tree().reload_current_scene()
+
+
+func _on_Button_pressed():
+	
