c50863f7f0b701a4725d41176a28bbdf7c6053a2
theczechr <robinjarusek@gmail.com>
2023-02-13 20:47:44 +0100

    fix wordle

--- minigames/wordle/Wordle.gd
+++ minigames/wordle/Wordle.gd
@@ -3,8 +3,11 @@ extends Control
 func _ready():
 	$AspectRatioContainer/line_container.connect("game_over", self, "game_over")
 
-func game_over(word):
-	$Label.text = word
+func game_over(word, guessed):
+	$AspectRatioContainer2/Label.text = word
 	$AnimationPlayer.play("fade_in")
 	yield(get_tree().create_timer(3), "timeout")
-	get_tree().change_scene("res://Level4.tscn")
+	if guessed:
+		get_tree().change_scene("res://Level4.tscn")
+	else:
+		get_tree().reload_current_scene()
